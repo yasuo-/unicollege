57a8a7167a11d617f5abbbf080d0112a066396e3
yasuo kurihara <breitling1216@gmail.com>
2021-09-17 07:53:33 +0900

    表示側更新

--- unicollege/students/urls.py
+++ unicollege/students/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('course/<pk>/',
          cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail'),
-    path('course/<pk>/<module_id>/',
-         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+    path('course/<pk>/<module_id>/', (views.StudentCourseDetailContentView.as_view()),
          name='student_course_detail_module'),
 ]
--- unicollege/students/views.py
+++ unicollege/students/views.py
@@ -74,3 +74,26 @@ class StudentCourseDetailView(DetailView):
             # get first module
             context['module'] = course.modules.all()
         return context
+
+
+class StudentCourseDetailContentView(DetailView):
+    """StudentCourseDetailView"""
+    model = Course
+    template_name = 'students/course/detail_content.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            # get first module
+            context['module'] = course.modules.all()
+        return context
