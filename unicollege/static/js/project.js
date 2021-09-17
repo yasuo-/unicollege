02408db258a144ac6b49fb9472516a6a000a5296
yasuo kurihara <breitling1216@gmail.com>
2021-09-17 10:32:27 +0900

    js修正

--- unicollege/static/js/project.js
+++ unicollege/static/js/project.js
@@ -1,1 +1,8 @@
 /* Project specific Javascript goes here. */
+$(".close").on("click", function(){
+  $("alert").remove();
+});
+
+$('.sidenav-trigger').on("click", function(){
+    $('.sidenav').css('transform', 'translateX(0%)');
+})
